af8ff01a70dc75e7eaf7f67234b28f76ec20a0ee
Andres Villa <pipevilla11@gmail.com>
2026-08-20 15:15:49 -0500

    Agrego componente VillaButton con link a YouTube

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Santiago from './components/Santiago'
 import Hero from './components/Hero'
+import VillaButton from './components/VillaButton'
 
 function App() {
 
@@ -12,11 +13,13 @@ function App() {
         <br />
         <Link to="/santiago"> Santiago </Link>
         <br />
-
+        
+        <VillaButton />
 
         <Routes>
           <Route path="/hero" element={<Hero />} />
           <Route path="/santiago" element={<Santiago />} />
+          
         </Routes>
         
       </BrowserRouter>
