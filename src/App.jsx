d0783bf4dd4c23e47b4bf71db38609b4e48156d2
estebantorresrd05 <estebantorresrd0506@gmail.com>
2026-08-20 15:43:00 -0500

    se agrega login

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,28 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Santiago from './components/Santiago'
 import Hero from './components/Hero'
+import Torres from './components/Torres'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-        <Link to="/hero"> Hero </Link>
-        <br />
-        <Link to="/santiago"> Santiago </Link>
+
+        <Link to="/hero">Hero</Link>
         <br />
 
+        <Link to="/santiago">Santiago</Link>
+        <br />
+
+        <Link to="/torres">Torres</Link>
 
         <Routes>
           <Route path="/hero" element={<Hero />} />
           <Route path="/santiago" element={<Santiago />} />
+          <Route path="/torres" element={<Torres />} />
         </Routes>
-        
+
       </BrowserRouter>
-
-
     </>
   )
 }
---
+++ src/components/Torres.jsx
@@ -0,0 +1,14 @@
+const Torres = () => {
+  return (
+    <div className="login">
+      <h2>Login</h2>
+
+      <input type="text" placeholder="Usuario" />
+      <input type="password" placeholder="Contraseña" />
+
+      <button>Iniciar sesión</button>
+    </div>
+  )
+}
+
+export default Torres
