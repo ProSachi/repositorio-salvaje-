2f1db626d2f4cc09f62260c128a3603fff59c961
alejandrotrujill00620-pixel <Alejandro.trujill0.0620@gmail.com>
2026-08-20 15:28:39 -0500

    entrega del componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,25 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Santiago from './components/Santiago'
 import Hero from './components/Hero'
+import Salchibro from './components/salchibro'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-        <Link to="/hero"> Hero </Link>
+        <Link to="/hero">Hero</Link>
         <br />
-        <Link to="/santiago"> Santiago </Link>
+        <Link to="/santiago">Santiago</Link>
         <br />
-
+        <Link to="/salchibro">Salchibro</Link>
+        <br />
 
         <Routes>
           <Route path="/hero" element={<Hero />} />
           <Route path="/santiago" element={<Santiago />} />
+          <Route path="/salchibro" element={<Salchibro />} />
         </Routes>
-        
       </BrowserRouter>
-
-
     </>
   )
 }
---
+++ src/components/Salchibro.jsx
@@ -0,0 +1,13 @@
+const Salchibro = () => {
+  return (
+    <div>
+      <h1>En el día más brillante, en la noche más oscura,ningún mal escapará de mi vista.Que aquellos que veneran el poder del mal,teman mi poder..</h1>
+      <img
+        src="https://static.wikia.nocookie.net/doblaje/images/3/37/Barry-LFDLSC.png/revision/latest?cb=20240711234602&path-prefix=es"
+        alt="Salchibro"
+      />
+    </div>
+  )
+}
+
+export default Salchibro
