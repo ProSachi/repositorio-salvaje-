4cc01de824648c837703175b10104dd7ee440f06
stivengilperez-source <stivengilperez@gmail.com>
2026-08-20 15:16:36 -0500

    jonatan

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Santiago from './components/Santiago'
 import Hero from './components/Hero'
+import jonatan from './components/jonatan'
 
 function App() {
 
@@ -12,11 +13,13 @@ function App() {
         <br />
         <Link to="/santiago"> Santiago </Link>
         <br />
+        <Link to="/jonatan"> Jonatan </Link>
 
 
         <Routes>
           <Route path="/hero" element={<Hero />} />
           <Route path="/santiago" element={<Santiago />} />
+          <Route path="/jonatan" element={<jonatan />} />
         </Routes>
         
       </BrowserRouter>
