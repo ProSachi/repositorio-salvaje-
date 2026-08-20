92ca47e2ccc58e1979d84dc6b831eb76f8a09723
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2026-08-20 14:57:33 -0500

    Se agrega el componente FooterAgd y su ruta

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Santiago from './components/Santiago'
 import Hero from './components/Hero'
+import FooterAgd from './components/FooterAgd'
 
 function App() {
 
@@ -12,11 +13,13 @@ function App() {
         <br />
         <Link to="/santiago"> Santiago </Link>
         <br />
+        <Link to="footeragd"> FooterAgd </Link>
 
 
         <Routes>
           <Route path="/hero" element={<Hero />} />
           <Route path="/santiago" element={<Santiago />} />
+          <Route path="/footeragd" element={<FooterAgd/>} />
         </Routes>
         
       </BrowserRouter>
