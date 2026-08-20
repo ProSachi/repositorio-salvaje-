7fa1df84e3ac5484f182050c96402237a747c603
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2026-08-20 15:45:55 -0500

    Se crea el footer

--- src/components/FooterAgd.jsx
+++ src/components/FooterAgd.jsx
@@ -1,6 +1,17 @@
+import './FooterAgd.css';
+
 const FooterAgd = () => {
   return (
-    <div>FooterAgd</div>
+    <footer className="footer">
+      <div className="contenido-footer">
+        <h3>Mi Sitio Web</h3>
+        <p>Todos los derechos reservados © 2026</p>
+        <ul className="links-footer">
+          <li><a href="#inicio">Inicio</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+      </div>
+    </footer>
   )
 }
 
