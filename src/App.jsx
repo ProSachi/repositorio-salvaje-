2698578e17e66c75f9da5c5d497a72448ebb9061
Caroortiz95 <caro.ortiz9501@gmail.com>
2026-08-20 14:58:30 -0500

    Feat saludo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Santiago from './components/Santiago'
 import Hero from './components/Hero'
+import Carolina from './components/Carolina'
 
 function App() {
 
@@ -12,11 +13,14 @@ function App() {
         <br />
         <Link to="/santiago"> Santiago </Link>
         <br />
+        <Link to="/carolina">Carolina</Link>
+        <br />
 
 
         <Routes>
           <Route path="/hero" element={<Hero />} />
           <Route path="/santiago" element={<Santiago />} />
+          <Route path="/carolina" element={<Carolina />} />
         </Routes>
         
       </BrowserRouter>
